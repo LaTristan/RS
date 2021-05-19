645dd05da05a9cf2c280a1174834b01e00c37f5f
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-05-19 16:07:07 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Factory.h 	modified:   main.cpp

--- Factory.h
+++ Factory.h
@@ -22,6 +22,9 @@ public:
             
         }
     }
+    void initialize(string input){
+        
+    }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -9,10 +9,57 @@ using namespace std;
 int main(){
     ifstream band1("band1.txt");
     if(!band1)return -1;
+    ifstream band2("band2.txt");
+    if(!band2)return -1;
+    ifstream band3("band3.txt");
+    if(!band3)return -1;
+    vector<int>tmp;
+    vector<vector<int>>b1;
+    vector<vector<int>>b2;
+    vector<vector<int>>b3;
     string s;
+    int t;
     while(getline(band1,s)){
-        cout<<s<<endl;
+        istringstream iss(s);
+        while(iss>>t){
+            tmp.push_back(t);
+        }
+        b1.push_back(tmp);
+        tmp.clear();
+    }
+    while(getline(band2,s)){
+        istringstream iss(s);
+        while(iss>>t){
+            tmp.push_back(t);
+        }
+        b2.push_back(tmp);
+        tmp.clear();
+    }
+    while(getline(band3,s)){
+        istringstream iss(s);
+        while(iss>>t){
+            tmp.push_back(t);
+        }
+        b3.push_back(tmp);
+        tmp.clear();
     }
     band1.close();
+    band2.close();
+    band3.close();
+    //BIL
+    for(int i=0;i<b1.size();i++){
+        for(int j=0;j<3;j++){
+            cout<<b1[i][j]<<"\t";
+        }
+        cout<<endl;
+        for(int j=0;j<3;j++){
+            cout<<b2[i][j]<<"\t";
+        }
+        cout<<endl;
+        for(int j=0;j<3;j++){
+            cout<<b3[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
     return 0;
 }
