7c47668ec84e253fb61ea90503c270a52f70afd4
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-05-19 15:31:45 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   Band.h 	modified:   Factory.h

---
+++ Band.h
@@ -0,0 +1,46 @@
+#ifndef _BAND_H_
+#define _BAND_H_
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Band{
+private:
+    vector<vector<int>> data;
+    int col=2147483647;
+    int row=2147483647;
+public:
+/*Input data must be like "12 34 56 78"*/
+    void importDataFromString(string import){
+        vector<int>tmp;
+        istringstream iss(import);
+        int r;
+        while(iss>>r){
+            tmp.push_back(r);
+        }
+        data.push_back(tmp);
+        tmp.clear();
+        row=data.size();
+        col=data[0].size();
+    }
+    /*Print information about this band*/
+    void printInfo(){
+        cout<<"********Info********"<<endl;
+        cout<<"Row:\t"<<row<<endl;
+        cout<<"Col:\t"<<col<<endl;
+    }
+    string getLineData(int n){
+        if(n>=row)return "Require out of bound.";
+        string res;
+        vector<int> tmp=data[n];
+        for(vector<int>::iterator it=tmp.begin();it!=tmp.end();it++){
+            res+=(" "+to_string(*it));
+        }
+        return res;
+    }
+};
+
+#endif
--- Factory.h
+++ Factory.h
@@ -1,9 +1,27 @@
 #ifndef _FACTORY_H_
 #define _FACTORY_H_
 
-#include <vector>
+#include "Band.h"
+#include <fstream>
 using namespace std;
 
-class Factory{};
+class Factory{
+    vector<Band>bands;
+    int bandNum=0;
+public:
+    void printInfo(){
+        cout<<"****Data Info****"<<endl;
+        cout<<"Bands Amount:"<<bandNum<<endl;
+        cout<<"Data Size:"<<bands.size();
+    }
+    /*Get BIL format data. Input file name or path/name*/
+    void convertBIL(string outputPath){
+        int n=bands.size();
+        ofstream ofs(outputPath+".txt");
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+};
 
 #endif
